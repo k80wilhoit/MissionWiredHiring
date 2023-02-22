e700ebd263cc792d15feed9f3aab31a7f3547742
k80wilhoit <111386699+k80wilhoit@users.noreply.github.com>
2023-02-22 15:42:42 -0500

    Add files via upload

--- Models/db.py
+++ Models/db.py
@@ -1,10 +1,8 @@
-import pandas as a
-data = pd.read_csv(r'C:Users/katiewilhoit/Downloads/cons.csv')
-df = pd.DataFrame(data, columns=[created_dt, updated_dt])
-import pandas as b 
-df = pd.DataFrame(data, columns=[email])
-data = pd.read_csv(r'C:Users/katiewilhoit/Downloads/cons_email.csv')
-import pandas as c 
-data = pd.read_csv(r'C:Users/katiewilhoit/Downloads/cons_email_chapter_subscription.csv')
-df = pd.DataFrame(data, columns=[is_unsub]) 
-print (df)
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
+
+db = SQLAlchemy()
